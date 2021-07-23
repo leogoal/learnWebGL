3071c86cc1a372ea43317c39070622fdb2e40627
lihao <1486600108@qq.com>
2021-07-23 14:56:11 +0800

    gl.clearColor gl.clear 2个API的理解

--- 02/02HelloCanvas.js
+++ 02/02HelloCanvas.js
@@ -13,7 +13,27 @@ function main() {
     }
 
     //指定清空<canvas>的颜色
+    /**
+     * 由于 WebGL 继承自 OpenGL ES，所以它遵循 OpenGl ES 颜色分量的取值范围（0.0 ~ 1.0）
+     * 一旦指定了背景色之后，背景色就会驻存在 WebGL 系统中，在下次调用 gl.clearColor 之前不会改变
+     * 也就是，如果背景色不变，该方法调用一次即可
+     */
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
     //清空<canvas>
+    /**
+     * 该方法继承自 OpenGL，它基于多基本缓冲区模型
+     * 参数：
+     *      gl.COLOR_BUFFER_BIT     指定颜色缓存区
+     *      gl.DEPTH_BUFFER_BIT     指定深度缓冲区
+     *      gl.STENCIL_BUFFER_BIT   指定模板缓冲区
+     */
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    /**
+     * 清空缓冲区的默认参数
+     *      颜色缓冲区：(0.0, 0.0, 0.0, 1.0)
+     *      深度缓冲区：1.0
+     *      模板缓冲区：0
+     */
 }
