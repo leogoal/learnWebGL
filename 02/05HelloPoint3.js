aa6e76cee5a314d989d96b865ecc66e4c36e34f1
lihao <1486600108@qq.com>
2021-07-26 09:54:03 +0800

    attribute变量实践

---
+++ 02/05HelloPoint3.js
@@ -0,0 +1,32 @@
+var VSHADER_SOURCE = 
+'attribute vec4 a_Position;\n' +
+'attribute float a_PointSize;\n' +
+'void main() {\n' +
+'   gl_Position = a_Position;\n' +
+'   gl_PointSize = a_PointSize;\n' +
+'}\n';
+
+var FSHADER_SOURCE = 
+'void main() {\n' +
+'   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+'}\n';
+
+function main() {
+    var canvas = document.getElementById('example');
+    var gl = getWebGLContext(canvas);
+
+    if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('初始化着色器失败');
+        return;
+    }
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+
+    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+    gl.vertexAttrib1f(a_PointSize, 20.0);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
