3efa911a0bbe96c2c39dd3c6b8b9b02e168c9932
lihao <1486600108@qq.com>
2021-07-26 10:02:19 +0800

    11

--- 02/05HelloPoint3.js
+++ 02/05HelloPoint3.js
@@ -2,13 +2,13 @@ var VSHADER_SOURCE =
 'attribute vec4 a_Position;\n' +
 'attribute float a_PointSize;\n' +
 'void main() {\n' +
-'   gl_Position = a_Position;\n' +
-'   gl_PointSize = a_PointSize;\n' +
+'gl_Position = a_Position;\n' +
+'gl_PointSize = a_PointSize;\n' +
 '}\n';
 
 var FSHADER_SOURCE = 
 'void main() {\n' +
-'   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+'gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
 '}\n';
 
 function main() {
@@ -24,9 +24,10 @@ function main() {
     var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
 
     gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
-    gl.vertexAttrib1f(a_PointSize, 20.0);
+    gl.vertexAttrib1f(a_PointSize, 5);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+
     gl.drawArrays(gl.POINTS, 0, 1);
 }
