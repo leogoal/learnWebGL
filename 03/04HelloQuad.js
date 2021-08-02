0f4bae9fda411492ed8dc6b4d7fc09f47b89a3d0
lihao <1486600108@qq.com>
2021-08-02 11:31:33 +0800

    11

--- 03/04HelloQuad.js
+++ 03/04HelloQuad.js
@@ -26,7 +26,7 @@ function main() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
 }
 
 function initVertexBuffers(gl) {
