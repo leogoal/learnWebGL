e4872c89cc28658586e055bf845ee87d443d7a49
lihao <1486600108@qq.com>
2021-08-11 10:50:44 +0800

    111

--- 05/01MultiAttributeSize.js
+++ 05/01MultiAttributeSize.js
@@ -1,4 +1,4 @@
-var VSHADER_SOURCE = 
+var VSHADER_SOURCE =
 'attribute vec4 a_Position;\n' +
 'attribute float a_PointSize;\n' +
 'void main() {\n' +
@@ -6,23 +6,20 @@ var VSHADER_SOURCE =
 '   gl_PointSize = a_PointSize;\n' +
 '}\n';
 
-var FSHADER_SOURCE = 
+var FSHADER_SOURCE =
 'void main() {\n' +
-'   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);' +
+'   gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' +
 '}\n';
 
 function main() {
     var canvas = document.getElementById('example');
     var gl = getWebGLContext(canvas);
-
     if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-        console.log('初始化着色器失败');
         return;
     }
 
     var n = initVertexBuffers(gl);
     if(n < 0) {
-        console.log('初始化顶点数据失败');
         return;
     }
 
@@ -33,15 +30,11 @@ function main() {
 }
 
 function initVertexBuffers(gl) {
-    /**
-     * 将几种“逐顶点”的数据（坐标 尺寸）交叉储存在一个数组中
-     */
     var vertices = new Float32Array([
         0.0, 0.5, 5.0,
         -0.5, -0.5, 10.0,
         0.5, -0.5, 20.0
     ])
-
     var n = 3;
 
     var vertexBuffer = gl.createBuffer();
@@ -50,11 +43,10 @@ function initVertexBuffers(gl) {
     }
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    
+
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
     var FSIZE = vertices.BYTES_PER_ELEMENT;
-
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 3, 0);
     gl.enableVertexAttribArray(a_Position);
@@ -65,4 +57,3 @@ function initVertexBuffers(gl) {
 
     return n;
 }
-
